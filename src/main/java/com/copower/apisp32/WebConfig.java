6ebfe4e8ff6a81a2047a58e0ff792acf24dc1959
diegodfp <diegodfp.fly@gmail.com>
2024-09-30 11:11:20 -0500

    cors

---
+++ src/main/java/com/copower/apisp32/WebConfig.java
@@ -0,0 +1,20 @@
+package com.copower.apisp32;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500", "https://sp32frontend.onrender.com/","https://cheery-dasik-60d356.netlify.app/") // Especifica tus orígenes
+                                                                                             // permitidos
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
